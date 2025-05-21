1df9b53971f52c47fdd33f5ce20d5ac4344311c4
Reagan Nyauma <jreagan2525@gmail.com>
2025-05-21 19:45:37 +0300

    completed tasks

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,27 +1,40 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function Form(){
+export default function Form() {
+  const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [focusing, setFocusing] = useState(false);
 
-    const [email, setEmail] = useState('')
-    const [submitted, setSubmitted] = useState(false);
+  useEffect(() => {
+    if (submitted) {
+      alert(`You have submitted email: ${email}`);
+      setSubmitted(false);
+    }
+  }, [submitted, email]); // React will render on every character change. BONUS: Improve this approach
 
-    useEffect(() => {
-        if(submitted){
-            alert(`You have submitted email: ${email}`)
-            setSubmitted(false)
-        }
-    }, [submitted, email]) // React will render on every character change. BONUS: Improve this approach
+  const clearInput = () => {
+    setEmail("");
+  };
 
-    return (
-        <div>
-            <h2>This is our controlled form</h2>
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                setSubmitted(true);
-            }}>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h2 className={focusing ? 'blur' : ''}>This is our controlled form</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setSubmitted(true);
+        }}
+      >
+        <input
+          className={focusing ? "focused" : ""}
+          onFocus={() => setFocusing(true)}
+          onBlur={() => setFocusing(false)}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+      <button onClick={clearInput}>Clear Input</button>
+    </div>
+  );
 }
