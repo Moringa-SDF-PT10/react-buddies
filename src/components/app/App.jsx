69afd3195b100c38988765ef9d6fbf6d3a435ddf
Levy Muchiri <levngi.muchiri@gmail.com>
2025-05-22 19:05:23 +0300

    completed tasks

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,6 +3,7 @@ import Form from '../Form'
 import Home from '../Home'
 import TaskList from '../TaskList'
 import './App.css'
+import MembersList from './membersList' 
 
 function App() {
 
@@ -13,12 +14,14 @@ function App() {
           <Link to={"/"}>Home</Link>
           <Link to={"/form"}>Form</Link>
           <Link to={"/tasks"}>Group Tasks</Link>
+          <Link to={"/membersList"}>Members List</Link>
         </nav>
         <main className='app-main'>
           <Routes>
             <Route path='/' element={ <Home/>} />
             <Route path='/form' element={ <Form/>} />
             <Route path='/tasks' element={ <TaskList/>} />
+            <Route path='/membersList' element={ <MembersList/>} />
           </Routes>
         </main>
       </div>
