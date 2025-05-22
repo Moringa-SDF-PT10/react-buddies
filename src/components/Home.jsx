f5c378f9e87019ddcc0a7523b85659f93c65b41b
Ike Mwithiga <mwithigaike@gmail.com>
2025-05-22 15:35:59 +0100

    feat: highlight input on focus and remove after 5s using useRef

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,19 @@ export default function Home(){
     const prevCount = useRef(0)
 
     const inputRef = useRef(null)
+    const removeTimeout = useRef(null); // Ref to store the timeout ID for blur effect
 
+  // Function to handle focusing the input and setting the highlight to green using useRef
+     const handleInputFocus = () => {
+     inputRef.current.style.setProperty("background-color", "lightgreen");
+};
+
+// Function to handle remove the green highlight on focus after a set delay using useRef
+    const handleRemoveTimeout = () => {
+    removeTimeout.current = setTimeout(() => {
+    inputRef.current.style.removeProperty("background-color");
+  }, 5000);
+};
     const handleClick = () => {
         inputRef.current?.focus()
     }
@@ -23,11 +35,17 @@ export default function Home(){
         <div>
             <h2>Welcome to the React Buddies page</h2>
             <p>This is a live collaborative demo for useRef hook:</p>
-            <input ref={inputRef} placeholder="Check me out" />
+            <input
+                ref={inputRef}
+                placeholder="Check me out"
+                onFocus={handleInputFocus}
+                onBlur={handleRemoveTimeout}
+            />
             <button onClick={handleClick}>Click Me</button>
             <div>
                 <p>Current Count: {count}</p>
                 <p>Previous Count: {prevCount.current}</p>
+
             <button onClick={increaseCount} >Increase button count</button>
             </div>
             {/* <div style={display.block}></div> */}
